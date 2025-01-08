c8d77f477f9fc340bd639d0439e1bc39915a41da
Afridi <afridi.hq97@gmail.com>
2025-01-08 20:36:45 +0530

    dockerfile added

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Ignore type errors during builds
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true, // Ignore type errors during builds
+  // },
 };
 
 export default nextConfig;
